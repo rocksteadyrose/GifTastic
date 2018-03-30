c6f38e3ce2d41cac7d63131a4ac2e90b8a52449a
Leslie Rose <leslierose@Leslies-MBP.attlocal.net>
2018-03-30 11:21:04 -0500

    styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
 
-var topics = ["full house", "roseanne", "the facts of life", "punky brewster", "the wonder years", "charles in charge", "diff'rent strokes", "family ties", "growing pains", "alf"];
+$(document).ready(function() {
+var topics = ["full house", "roseanne", "the facts of life", "punky brewster", "the wonder years", "charles in charge", "diff'rent strokes", "family ties", "growing pains", "alf", "dinosaurs", "saved by the bell"];
 var newTopics = [];
     
   clickButtons();
@@ -8,16 +8,16 @@ var newTopics = [];
         
     for (i=0; i<topics.length; i++) {
 
-        if (topics.length === 10) {
+        if (topics.length === 12) {
          showButton = $("<button>");
          showButton.text(topics[i]);
          var string = JSON.stringify(topics[i]);
          var stringReplace = string.replace(/\"/g, "");
          showButton.attr("data-button", stringReplace);
-       $("#80sbuttons").append(showButton);}
+       $("#buttons80s").append(showButton);}
         }
 
-       if (topics.length > 10) {
+       if (topics.length > 12) {
         var newShow = topics.slice(-1)[0];
         newTopics.push(newShow);
         for (i=0; i<newTopics.length; i++) {
@@ -27,7 +27,7 @@ var newTopics = [];
         var string = JSON.stringify(newTopics[i]);
         var stringReplace = string.replace(/\"/g, "");
         showButton.attr("data-button", stringReplace);
-        $("#80sbuttons").append(showButton);
+        $("#buttons80s").append(showButton);
         topics.push(newShow);
         newTopics = [];
     }
@@ -43,10 +43,6 @@ var newTopics = [];
         var buttonEntry = show.split(' ').join('+');
 
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + buttonEntry + "&api_key=mcfoMoI0yxAtwb46m31d1FWrixVlfsL4&limit=10";
-
-         
-
-    // var searchTerm = $("#shows-input").val().trim();
     
         $.ajax({
          url: queryURL,
@@ -55,22 +51,22 @@ var newTopics = [];
    
        .then(function(response) {
    
-        var gifDiv = $("<div class='chosengif'>");
-
         var results = response.data;
         console.log(response.data);
 
         for (i=0; i<results.length; i++) {
 
+        var gifDiv = $("<div class='chosengif'>");
+
         var rating = results[i].rating;
 
-        var p = $("<p>").text("Rating: " + rating);
+        var h = $("<h3>").text("Rating: " + rating);
 
         var gif = $("<img>").attr({"src": results[i].images.fixed_height_still.url, "data-still": results[i].images.fixed_height_still.url, "data-animate": results[i].images.fixed_height.url,
         "data-state": "still"});
         gif.addClass('newgif');
         
-        gifDiv.append(p);
+        gifDiv.append(h);
         gifDiv.append(gif);
 
         $("#showsDiv").append(gifDiv);
@@ -85,10 +81,6 @@ var newTopics = [];
    event.preventDefault();
    //The trim() method removes whitespace from both sides of a string.
    var showName = $("#shows-input").val().trim();
-   console.log(showName);
-
-  // buttonEntry = show.split(' ').join('+');
-
    topics.push(showName);
    clickButtons(showName);
 })
@@ -96,7 +88,7 @@ var newTopics = [];
 $(document).on('click', '.newgif', function() {
     var state = $(this).attr("data-state");
     console.log(this);
-
+ 
 if (state === "still") {
         $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate");
